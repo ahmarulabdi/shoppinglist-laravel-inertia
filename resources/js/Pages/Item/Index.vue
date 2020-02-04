218a3e6abca8950ac57d164e1536a2cb9677317a
Ahmarul Abdi <abdi@dropshipkit.com>
2020-02-04 16:57:14 +0700

    adding item index

---
+++ resources/js/Pages/Item/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <table border="1">
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+            <tr v-for="item in items">
+                <td>{{ item.name }}</td>
+                <td>{{ item.description }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    import Layout from '../../Shared/Layout'
+    export default {
+        name: "Index",
+        layout: Layout,
+        props: {
+            'items': Array
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
