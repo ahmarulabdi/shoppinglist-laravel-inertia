2c1cb7e44ba2d53f77ac37b88589ad732d390cf8
Ahmarul Abdi <abdi@dropshipkit.com>
2020-02-04 16:56:48 +0700

    adding navbar testing

---
+++ resources/js/Shared/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li>link 1</li>
+            <li>link 2</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
