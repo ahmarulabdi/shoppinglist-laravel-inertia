66aafcb641d288f27e46ae2d0dd27fa140982d7d
Ahmarul Abdi <abdi@dropshipkit.com>
2020-02-04 09:27:47 +0700

    setup inertia in client side

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+import { InertiaApp } from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+
+Vue.use(InertiaApp)
+
+const app = document.getElementById('app')
+
+new Vue({
+    render: h => h(InertiaApp, {
+        props: {
+            initialPage: JSON.parse(app.dataset.page),
+            resolveComponent: name => require(`./Pages/${name}`).default,
+        }
+    })
+}).$mount(app)
