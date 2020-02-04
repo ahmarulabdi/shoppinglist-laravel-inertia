0b031d4cdb68b9eda7b81008c3bb1cd3279ce8d2
Ahmarul Abdi <abdi@dropshipkit.com>
2020-02-04 16:57:04 +0700

    adding layout master

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <slot/>
+    </div>
+</template>
+
+<script>
+    import Navbar from "./Navbar"
+    export default {
+        components :{
+            Navbar,
+        },
+        name: "Layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
