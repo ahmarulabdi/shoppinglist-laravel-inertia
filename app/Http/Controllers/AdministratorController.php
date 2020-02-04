dd440295147b0649a5a7b57a7509271f62780b2a
Ahmarul Abdi <abdi@dropshipkit.com>
2020-02-04 09:25:38 +0700

    example response using inertia render

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+
 class AdministratorController extends Controller
 {
     public function index()
     {
-
+        return Inertia::render('Administrator/Index',[
+            'title' => 'Welcome',
+            'description' => 'Welcome to the club'
+        ]);
     }
 }
