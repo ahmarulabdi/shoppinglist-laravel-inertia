5e821eb69be248bb51cade01844cd18fedd96804
Ahmarul Abdi <abdi@dropshipkit.com>
2020-02-04 10:17:21 +0700

    adding file

---
+++ resources/js/Pages/Administrator/Index.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+       <button class="btn btn-info">tes</button>
+       <ul>
+           <li>Title : {{ title }}</li>
+           <li>Description : {{ description }}</li>
+       </ul>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        props: {
+            'title': String,
+            'description' : String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
